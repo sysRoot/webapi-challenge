0eb2fdba7ab8cab968cd5e69011996b6f1db1ea0
Jason Cook <the.root.user@gmail.com>
2019-08-04 23:45:31 -0400

    Delete and Put at /projects

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -9,17 +9,6 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({ error: "The project information could not be retrieved."}))
 })
 
-router.get('/:id', async (req, res) => {
-  try {
-    const project = await projectModel.get(req.params.id);
-    project
-      ? res.status(200).json(project)
-      : res.status(404).json({ message: "A project with that ID could not be found."})
-  }
-  catch (err) {
-    res.status(500).json({ error: "The project information could not be retrieved."})
-  }
-})
 
 router.get('/:id', async (req, res) => {
     try {
@@ -44,4 +33,32 @@ router.get('/:id', async (req, res) => {
       }
     }
   })
+  
+  router.delete('/:id', async (req, res) => {
+    try {
+      const count = await projectModel.remove(req.params.id);
+      count
+      ? res.status(200).json({ message: "The project was deleted.", id: `${req.params.id}`})
+      : res.status(404).json({ message: "A user with that ID could not be found."})
+    } catch (err) {
+      res.status(500).json({ error: 'Could not complete request.'})
+    }
+  })
+  
+  router.put('/:id', async (req, res) => {
+    if (req.body.name && req.body.name.length < 129 && req.body.description){
+      try {
+        const count = await projectModel.update(req.params.id, req.body);
+        if (count) {
+          const project = await projectModel.get(req.params.id);
+          res.status(200).json(project);
+        } else {
+          res.status(404).json({ message: "A project with that ID could not be found."})
+        }
+      } catch (err) {
+        res.status(500).json({ error: "The project could not be updated."})
+      }
+    }
+  })
+
 module.exports = router;
